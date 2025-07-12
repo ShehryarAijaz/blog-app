d840a9c621644c2aa3d925b8d20ce36427aa13f3
Shehryar Aijaz <shehryar120345@gmail.com>
2025-07-13 02:52:39 +0500

    feat: integrate Tailwind CSS and implement authentication with Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth.service.js'
+import { login, logout } from './store/authSlice.js'
+import { Footer, Header } from './components/index.js'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-200">
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
+  
 }
 
 export default App
